6254d167396824caa05b62afa70f8127fe96e08f
Simon Trepanier <2492224@bdeb.qc.ca>
2026-02-05 14:02:15 -0500

    creation du projet

---
+++ Fenetre.cpp
@@ -0,0 +1,31 @@
+#include "Fenetre.h"
+
+namespace app {
+	bool Fenetre::create_window(int largeur, int hauteur, const char* titre) {
+		HINSTANCE instance = GetModuleHandleA(0);
+
+		WNDCLASSA ws = {};
+		ws.hInstance = instance;
+		ws.hIcon = LoadIcon(instance, IDI_APPLICATION);
+		ws.hCursor = LoadCursor(NULL, IDC_ARROW);
+		ws.lpszClassName = titre;
+		ws.lpfnWndProc = DefWindowProcA;
+
+		if (!RegisterClassA(&ws)) {
+			return false;
+		}
+
+		int style = WS_OVERLAPPEDWINDOW;
+
+		HWND window = CreateWindowExA(0, titre, titre,
+			style, 100, 100, largeur, hauteur, NULL, NULL, instance, NULL);
+
+		if (window == NULL) {
+			return false;
+		}
+
+		ShowWindow(window, SW_SHOW);
+
+		return true;
+	}
+}
---
+++ Fenetre.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <Windows.h>
+namespace app {
+
+	class Fenetre {
+	public:
+		bool create_window(int, int, const char*);
+	};
+}
